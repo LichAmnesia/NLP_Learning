c3f9182092a667b5dcb68524427f00502694f140
Lich_Amnesia <alwaysxiaop@gmail.com>
2016-09-01 11:51:05 -0600

    for test in win

---
+++ fortest.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Author: Lich_Amnesia
+# @Email: alwaysxiaop@gmail.com
+# @Date:   2016-09-01 11:50:12
+# @Last Modified time: 2016-09-01 11:50:35
+# @FileName: fortest.py
